6f1a7a3a7cecfcf27a441d3a0b92f5ada328f2ea
firatdalkilic <104427724+firatdalkilic@users.noreply.github.com>
2023-08-21 21:40:27 +0300

    first commit

---
+++ movies/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path
+from .views import *
+
+urlpatterns = [
+    path('', index, name='index'),
+    path('movies/<int:id>', movies, name='movies'),
+    path('search/', search, name='search')
+]
---
+++ user/urls.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from django.urls import path
+from .views import *
+
+urlpatterns = [
+    path('register/', userRegister, name='register'),
+    path('login/', userLogin, name='login'),
+    path('profiles/', profiles, name='profiles'),
+    path('create/', createProfil, name='create'),
+    path('logout', userLogout, name='logout'),
+    path('hesap/', hesap, name='hesap'),
+    path('delete/', userDelete, name='delete')
+]
